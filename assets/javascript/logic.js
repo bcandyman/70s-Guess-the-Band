d7dfc66d776a75032418af3dbd4d11064137c68b
Benjamin Candy <bcandy93@gmail.com>
2019-10-01 02:17:06 -0700

    added mp3 music files. Added audio tag in index.html file. Added audio support to logic.js file.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,12 +1,27 @@
 // Variables
 var bandNames = [
+    "AC/DC",
+    "Black Sabbath",
+    "Deep Purple",
     "Iron Maiden",
     "Judas Priest",
+    "Led Zeppelin",
     "Motorhead",
+    "Rainbow",
     "Rush",
-    "Scorpions",
-    "Thin Lizzy"
 ];
+
+var bandSong = {
+    "AC/DC" : "High Voltage",
+    "Black Sabbath" : "Children of the Grave",
+    "Deep Purple"   : "Space Truckin'",
+    "Iron Maiden"   : "Hallowed Be Thy Name",
+    "Judas Priest"  : "Victim of Changes",
+    "Led Zeppelin"  : "Black Dog",
+    "Motorhead"     : "No Class",
+    "Rainbow"       : "Man On the Silver Mountain",
+    "Rush"          : "Working Man"
+}
 
 var chosenBandName;
 var guessedCharacters = ["a"] ;
@@ -15,9 +30,17 @@ var inGame = false ;
 var wins = 0 ;
 var correctCharacters = [] ;
 var incorrectCharacters = [] ;
+var audio ;
+
+var x = document.getElementById("myAudio"); 
 
 
-// Functions
+
+
+// +++++Functions+++++++
+
+//Tests if string is present in array
+//arr = array, char = string to find, addToArray is boolean and adds to array if true
 function inArr(arr,char,addToArray){
     var wasInArr = arr.includes(char) ;
     if (arr.includes(char)){
@@ -29,6 +52,8 @@ function inArr(arr,char,addToArray){
     return wasInArr
 }
 
+//Populates ID tag in html document
+//str = string to be displayed in html, ID = ID tag in html, appendMode is boolean and adds to end of current content if true
 function populateID (str, ID, appendMode){
     if (appendMode){
         document.getElementById(ID).innerHTML = document.getElementById(ID).innerHTML + str ;
@@ -38,18 +63,49 @@ function populateID (str, ID, appendMode){
     }
 }
 
+//Resets variables and html content
 function clearDocument(){
     inGame = false ;
     chosenBandName = "" ;
     correctCharacters = [] ;
     incorrectCharacters = [] ;
+    guessesRemaining = 6 ;
     populateID("", "currentWord", false) ;
     populateID("", "guessedCharacters", false) ;
     populateID("", "guessesRemaining", false) ;
     document.getElementById("startGame").style.visibility = "initial" ;
-
-    // document.location.reload(false) ;
+    // x.pause()
+    stopAudio()
 }
+
+//Plays audio
+//audioFileName = file name only. Audio files must be in audio folder.
+function playAudio(audioFileName){
+    document.getElementById("myAudio").src = "assets/audio/" + audioFileName + ".mp3"
+    x.volume=1;
+    x.play();
+}
+
+//Fades audio out before pausing
+function stopAudio(){
+    for(i=1;i>0; i-= .01){
+        sleep(10) ;
+        console.log(i) ;
+        x.volume=i ;
+    }
+    x.pause()
+}
+
+//Stalls reader in function for given time
+//milliseconds = Amount of time to stall..
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds){
+            break;
+        }
+        }
+    }
 
 
 //
@@ -60,6 +116,7 @@ document.onkeypress = function(event) {
 
     //Starts game if not already started
     if (inGame === false && userGuess === " ") {
+        clearDocument()
         inGame = true;
         //chooses word to guess
         chosenBandName = bandNames[Math.floor(Math.random() * bandNames.length)];
@@ -68,6 +125,10 @@ document.onkeypress = function(event) {
             if (chosenBandName.charAt(i) === " "){
                 console.log("band name has a space")
                 populateID(" ", "currentWord", true)
+            }
+            else if (chosenBandName.charAt(i) === "/"){
+                console.log("band name has a slash")
+                populateID("/", "currentWord", true)
             }
             else{
                 populateID("_", "currentWord", true)
@@ -99,6 +160,9 @@ document.onkeypress = function(event) {
                         else if (character === " "){
                             populateID(" ", "currentWord", true)
                         }
+                        else if (character === "/"){
+                            populateID("/", "currentWord", true)
+                        }
                         else{
                             populateID("_", "currentWord", true)
                         }
@@ -114,26 +178,32 @@ document.onkeypress = function(event) {
                         populateID(", ", "guessedCharacters", true)
                     }
                     populateID(incorrectCharacters[incorrectCharacters.length - 1], "guessedCharacters", true)
-
-                    if (guessesRemaining === 0){
-                        alert("Brr... You ran out of tries. Please try again.")
-                        clearDocument()
-                    }
                     //decrement guessesRemaining and update html document
-                    guessesRemaining--
-                    populateID(guessesRemaining, "guessesRemaining", false)
+                    guessesRemaining-- ;
+                    populateID(guessesRemaining, "guessesRemaining", false) ;
+                    if (guessesRemaining === 0){
+                        alert("Brr... You ran out of tries. Please try again.") ;
+                        clearDocument() ;
+                    }
                 }
             }
         }
     }
     if (chosenBandName === document.getElementById("currentWord").innerHTML){
-        alert("You did it!!")
-        clearDocument()
+        console.log(chosenBandName)
+        console.log(bandSong[chosenBandName])
+        playAudio(bandSong[chosenBandName])
         wins++
         populateID(wins, "wins", false)
+        inGame = false ;
+        document.getElementById("startGame").style.innerHTML = "PRESS SPACE BAR TO PLAY AGAIN!!"
+        document.getElementById("startGame").style.visibility = "initial"
+
+        // clearDocument()
+        // playAudio(bandSong[chosenBandName])
     }
 }
 
-document.getElementById("wins").innerHTML = wins ;
+// document.getElementById("wins").innerHTML = wins ;
 // document.getElementById("guessesRemaining").innerHTML = guessesRemaining ;
 // document.getElementById("guessedCharacters").innerHTML = guessedCharacters[0] ;
